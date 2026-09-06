acb38d4f907f701c2b075f9e71be21ead9dff089
stranskyrichard00 <160427857+stranskyrichard00@users.noreply.github.com>
2026-09-06 01:06:02 +0000

    Updated Ascii_Art

---
+++ labs/L0-stdio/echoechoecho/src/echoechoecho.cpp
@@ -0,0 +1,33 @@
+/*
+    StdIO Lab
+    Kattis - Echo Echo Echo Problem
+
+    Updated By: <FIXME>
+    Date: <FIXME>
+    CSCI 111
+
+    This program prints the input word 3 times.
+
+    Algorithm steps:
+    1. Declare a string variable to store input data
+    3. Read and store the input data into the variable
+    3. Print the word 3 times separated by space
+*/
+
+#include <iostream>
+// FIXME1: include string library
+
+// FIXME2: use namespace standard
+
+int main()
+{
+    // FIXME3: Uncomment the following two lines
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    // FIXME4: algorithm step 1
+    // FIXME5: algorithm step 2
+    // FIXME6: algorithm step 3
+
+    return 0;
+}
